a4eca30feabb7374fcfc98c632dfe0146e736a12
Vladyslav Pienov <dontmyak@gmail.com>
2025-10-23 18:52:55 +0300

    testcontainers (v1)

---
+++ cmd/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"context"
+	"github.com/joho/godotenv"
+	"os"
+	"os/signal"
+	"syscall"
+
+	_ "github.com/lib/pq"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+
+	"UserLIST/pkg/handler"
+	"UserLIST/pkg/repository"
+	"UserLIST/pkg/service"
+	"UserLIST/todo"
+)
+
+// @title User List API
+// @version 1.0
+// @description API Server for User List Application
+
+// @host localhost:8000
+// @BasePath /
+
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name Authorization
+
+func main() {
+	logrus.SetFormatter(new(logrus.JSONFormatter))
+
+	if err := godotenv.Load(); err != nil {
+		logrus.Warnf("error loading .env variables: %s ", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     os.Getenv("HOST"),
+		Port:     os.Getenv("DB_PORT"),
+		Username: os.Getenv("POSTGRES_USER"),
+		DBName:   os.Getenv("POSTGRES_DB"),
+		SSLMode:  os.Getenv("DB_SSLMODE"),
+		Password: os.Getenv("POSTGRES_PASSWORD"),
+	})
+	if err != nil {
+		logrus.Fatalf("failed to initialize db: %s", err.Error())
+	}
+	repos := repository.NewRepository(db)
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
+	srv := new(todo.Server)
+	go func() {
+		if err := srv.Run(viper.GetString("APP_PORT"), handlers.InitRoutes()); err != nil {
+			logrus.Fatalf("error occured while running http server: %s", err.Error())
+		}
+	}()
+
+	logrus.Print("UserList Started")
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	<-quit
+
+	logrus.Print("UserList Shutting Down")
+
+	if err := srv.Shutdown(context.Background()); err != nil {
+		logrus.Errorf("error occured on server shutting down: %s", err.Error())
+	}
+
+	if err := db.Close(); err != nil {
+		logrus.Errorf("error occured on db connection close: %s", err.Error())
+	}
+}
